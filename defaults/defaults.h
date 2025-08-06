d282d6dbe69a9daa2114aeb0f769fbc01574a089
Florian Didron <fd@0x6664.com>
2025-08-06 09:27:15 +0700

    fix: #pragma once

--- defaults/defaults.h
+++ defaults/defaults.h
@@ -1,6 +1,7 @@
 // Copyright 2024 ZSA Technology Labs, Inc <@zsa>
 // SPDX-License-Identifier: GPL-2.0-or-later
 //
+#pragma once
 #include "quantum.h"
 // List of ZSA specific keycodes, appended to the end of the keycode list.
 enum zsa_keycodes {
