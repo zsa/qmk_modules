87dfa4e753ecd857158760a0d2c6c10a3c72aba6
phewphewb <valentyn.patsera@gmail.com>
2026-01-25 20:35:58 -0500

    add active layer raw hid push for klayi app

---
+++ klayi/active_layer_indicator.h
@@ -0,0 +1,12 @@
+// Copyright 2025 Nomisolutions
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include "quantum.h"
+
+#define ACTIVE_LAYER_INDICATOR_MESSAGE 0xA5
+
+enum active_layer_indicator_command_id {
+    ACTIVE_LAYER_PUSH = 0x01,
+};
